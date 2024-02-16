884add88dba8f0203d43454c8e04f2822360edf9
julia <julia.light@mymail.champlain.edu>
2024-02-16 11:24:23 -0500

    new commit

---
+++ Parsing Apache Lab.ps1
@@ -0,0 +1,23 @@
+﻿clear
+
+function ApacheLogs1(){
+$logsNotFormatted = Get-Content C:\xampp\apache\logs\access.log
+$tableRecords = @()
+
+for($i=0; $i -in $logsNotFormatted; $i++){
+$words = $logsNotFormatted[$i].Count(" ");
+
+$tableRecords +=[pscustomobject]@{ "IP" = $words[$i];`
+                                   "Time" = $words[$i].Trim('[');`
+                                   "Method" = $words[$i].Trim('"');`
+                                   "Page" = $words[$i];`
+                                   "Protocol" = $words[$i];`
+                                   "Response" = $words[$i];`
+                                   "Referrer" = $words[$i];`
+                                   "Client" = $words[11..($words - 1)]; }
+}
+
+return $tableRecords | Where-Object{$_.IP -like "10.*"}
+}
+$tableRecords = ApacheLogs1
+$tableRecords = Format-Table -AutoSize -Wrap
---
+++ html elements and web requests.ps1
@@ -0,0 +1,22 @@
+﻿clear
+
+$scraped_page = Invoke-WebRequest -TimeoutSec 10 http://10.0.17.35/ToBeScraped.html
+
+# Display links as HTML Element
+$scraped_page.Links
+
+# Get a count of the links in the page
+$scraped_page.count
+
+# Display only URL and its text
+$scraped_page.Links | Select -ExpandProperty href
+
+# Get outer text of every element with the tag h2
+$h2s=$scraped_page.ParsedHtml.body.getElementsByTagName("h2") | select outerText
+
+$h2s
+
+# 13. Print innerText of every div element that has the class as “div-1”
+$divs1=$scraped_page.ParsedHtml.body.getElementsByTagName("div") | where {$_.getattributeNode("class").value -ilike "div-1"} | select innertext
+
+$divs1
