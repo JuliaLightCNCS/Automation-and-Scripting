577d28e4cd6f42afab56c0f273632317ff30fd5c
julia <julia.light@mymail.champlain.edu>
2024-02-18 13:15:30 -0500

    new changes

---
+++ week 5/ScrapingChamplainClasses.ps1
@@ -0,0 +1,58 @@
+﻿clear
+
+function gatherClasses(){
+$page = Invoke-WebRequest -TimeoutSec 2 http://10.0.17.35/courses.html
+
+# Get all the tr elements of html document
+$trs=$page.parsedHTML.body.getelementsbytagname("tr")
+
+#empty array to hold results
+$fulltable = @()
+for($i=1; $i -lt $trs.length; $i++){ # Going over every tr element
+
+    # Get every td element of current tr element
+    $tds=$trs[$i].getelementsbytagname("td")
+
+    # Want to separate start time and end time from one time field
+    $Times = $tds[5].innertext.split("-")
+
+    $fulltable +=[pscustomobject]@{"Class Code" = $tds[0].innertext; `
+                                   "Title" = $tds[1].innertext; `
+                                   "Days" = $tds[4].innertext; `
+                                   "Time start" = $Times[0]; `
+                                   "Time end" = $Times[1]; `
+                                   "Instructor" = $tds[6].innertext; `
+                                   "location" = $tds[9].innertext;
+                                        }
+    }
+return $fulltable
+}
+
+
+function daysTranslator($fulltable) {
+
+    # Go over every record in the table
+    for ($i=0; $i -lt $fulltable.length; $i++) {
+        # Empty array to hold days for every record
+        $days = @()
+
+        # "M" for Monday
+        if ($fulltable[$i].Days -ilike "*M*"){ $days += "Monday" }
+
+        # "T" Followed T, W, or F for Tuesday
+        if ($fulltable[$i].Days -ilike "*T[TWF]*"){ $days += "Tuesday" }
+
+        # "W" for Wednesday
+        if ($fulltable[$i].Days -ilike "*W*"){ $days += "Wednesday" }
+
+        # "TH" for Thursday
+        if ($fulltable[$i].Days -ilike "*TH*"){ $days += "Thursday" }
+
+        # "F" for Friday
+        if ($fulltable[$i].Days -ilike "*F*"){ $days += "Friday" }
+
+        $fulltable[$i].Days = $days
+
+    }
+    return $fulltable
+}
---
+++ week 5/dotnotation.ps1
@@ -0,0 +1,26 @@
+﻿clear
+
+cd $PSScriptRoot
+
+. .\ScrapingChamplainClasses.ps1
+
+$table = gatherClasses
+
+$fulltable = daysTranslator $table
+
+# $fulltable | Select-Object "Class-Code", Instructor, Location, Days, "Time Start", "Time End" | ? {$_."Instructor" -ilike "Furkan*"}
+
+# $fulltable | ? { ($_.Location -ilike "JOYC 310") -and($_.Days -contains "Monday") } | Sort-Object "Time Start" | Select-Object "Time Start", "Time End", "Class Code"
+
+$ITSInstructors = $fulltable | Sort-Object { ($_."Class Code" -ilike "SYS*") -or `
+                                   ($_."Class Code" -ilike "NET*") -or `
+                                   ($_."Class Code" -ilike "SEC*") -or `
+                                   ($_."Class Code" -ilike "FOR*") -or `
+                                   ($_."Class Code" -ilike "CSI*") -or `
+                                   ($_."Class Code" -ilike "DAT*") } `
+                             | Select-Object "Instructor" `
+                             | Sort-Object "Instructor" -Unique
+
+$fulltable | ? { $_.Instructor -in $ITSInstructors.Instructor } `
+           | Group-Object "Instructor" | Select-Object Count, Name `
+           | Sort-Object Count -Descending
